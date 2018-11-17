e059a9193229c92984902d759656f770cf0f3959
Bogdan <burcea.bogdan.madalin@gmail.com>
2018-11-17 21:52:03 +0200

    Added Recommendations

--- src/main/java/ro/cburcea/hackathon/webapp/controllers/CompanyController.java
+++ src/main/java/ro/cburcea/hackathon/webapp/controllers/CompanyController.java
@@ -12,8 +12,7 @@ import ro.cburcea.hackathon.webapp.entities.Review;
 import ro.cburcea.hackathon.webapp.repositories.CompanyRepository;
 import ro.cburcea.hackathon.webapp.repositories.ReviewRepository;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 @RestController
 public class CompanyController {
@@ -60,6 +59,97 @@ public class CompanyController {
 //        return arr;
     }
 
+    private static class Pair<T1, T2> {
+        T1 first;
+        T2 second;
+        Pair(T1 a, T2 b) {
+            first = a;
+            second = b;
+        }
+    };
+
+    private static class Comp implements Comparator<Pair<Company, Double>> {
+        public int compare(Pair<Company, Double> a, Pair<Company, Double> b) {
+            return Double.compare(a.second, b.second);
+        }
+    }
+
+    @GetMapping(path = "/companies/recommendations", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public List<Company> getRecomandation(@RequestParam(value = "salary", required = false) Long salary,
+                                          @RequestParam(value = "interview", required = false) Long interviews,
+                                          @RequestParam(value = "env", required = false) Long environment,
+                                          @RequestParam(value = "training", required = false) Long trainings,
+                                          @RequestParam(value = "tag", required = false) String tag) {
+
+        ArrayList<String> categories = new ArrayList<>();
+        categories.add("Salaries");
+        categories.add("Interviews");
+        categories.add("Environment");
+        categories.add("Trainings");
+        Long gradeTotal = salary + interviews + environment + trainings;
+        TreeMap<String, Double> weight = new TreeMap<>();
+        weight.put("Salaries", (double)salary / gradeTotal);
+        weight.put("Interviews", (double)interviews / gradeTotal);
+        weight.put("Environment", (double)environment / gradeTotal);
+        weight.put("Trainings", (double)trainings / gradeTotal);
+
+        List<Review> reviews = reviewRepository.findAll();
+        TreeMap<Long, ArrayList<Review>> reviewMap = new TreeMap<>();
+        TreeMap<Long, Company> idToCompany = new TreeMap<>();
+
+        for (Review r : reviews) {
+            if (tag != null && r.getCompany().getTag().getName().equals(tag) == false) {
+                continue;
+            }
+
+            Long id = r.getCompany().getId();
+            if (reviewMap.get(id) == null) {
+                reviewMap.put(id, new ArrayList<Review>());
+            }
+            reviewMap.get(id).add(r);
+            idToCompany.put(id, r.getCompany());
+        }
+
+        ArrayList<Pair<Company, Double>> sorted = new ArrayList<>();
+        for (Map.Entry< Long, ArrayList<Review> > e : reviewMap.entrySet()) {
+            TreeMap<String, Integer> categorySum = new TreeMap<>();
+            TreeMap<String, Integer> categoryNum = new TreeMap<>();
+
+            for (String c : categories) {
+                categorySum.put(c, 0);
+                categoryNum.put(c, 0);
+            }
+
+            for (Review r : e.getValue()) {
+                String cat = r.getCategory();
+                categorySum.put(cat, categorySum.get(cat) + r.getRating());
+                categoryNum.put(cat, categoryNum.get(cat) + 1);
+            }
+
+            double weightedAverage = 0.0;
+            for (String cat : categories) {
+                Integer total = categorySum.get(cat), num = categoryNum.get(cat);
+                if (num == 0) {
+                    continue;
+                }
+
+                weightedAverage += weight.get(cat) * ((double)total / num);
+            }
+
+            Long id = e.getKey();
+            sorted.add(new Pair<Company, Double>(idToCompany.get(id), weightedAverage));
+        }
+
+        Collections.sort(sorted, new Comp());
+        Collections.reverse(sorted);
+
+        ArrayList<Company> ret = new ArrayList<>();
+        for (Pair<Company, Double> p : sorted) {
+            ret.add(p.first);
+        }
+
+        return ret;
+    }
 
     @GetMapping(path = "/companies/{id}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public Company getCompaniesById(@PathVariable Long id) {
