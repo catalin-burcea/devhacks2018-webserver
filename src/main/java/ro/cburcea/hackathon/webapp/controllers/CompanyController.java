a3c005abb940255bc7f91bc901a2ddbd0a04a41c
Bogdan <burcea.bogdan.madalin@gmail.com>
2018-11-17 17:50:37 +0200

    Added getCompanyByName

--- src/main/java/ro/cburcea/hackathon/webapp/controllers/CompanyController.java
+++ src/main/java/ro/cburcea/hackathon/webapp/controllers/CompanyController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import ro.cburcea.hackathon.webapp.demos.ObjectNotFoundException;
 import ro.cburcea.hackathon.webapp.entities.Company;
@@ -18,8 +19,11 @@ public class CompanyController {
     CompanyRepository companyRepository;
 
     @GetMapping(path = "/companies", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public List<Company> getCompanies() {
-        return companyRepository.findAll();
+    public List<Company> getCompanies(@RequestParam(value = "name", required = false) String name) {
+        if (name == null) {
+            return companyRepository.findAll();
+        }
+        return companyRepository.findByName(name);
     }
 
 
@@ -27,4 +31,12 @@ public class CompanyController {
     public Company getCompaniesById(@PathVariable Long id) {
         return companyRepository.findById(id).orElseThrow(() -> new ObjectNotFoundException(id));
     }
+
+//    @GetMapping(path = "/companies/{name}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+//    public List<Company> getCompaniesByName(@PathVariable String name) {
+//        List<Company> ret = companyRepository.findByName(name);
+//        return ret;
+//    }
+
+
 }
--- src/main/java/ro/cburcea/hackathon/webapp/repositories/CompanyRepository.java
+++ src/main/java/ro/cburcea/hackathon/webapp/repositories/CompanyRepository.java
@@ -3,5 +3,8 @@ package ro.cburcea.hackathon.webapp.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ro.cburcea.hackathon.webapp.entities.Company;
 
+import java.util.List;
+
 public interface CompanyRepository extends JpaRepository<Company, Long> {
+    List<Company> findByName(String name);
 }
