0208f72d25c0a0501e7a421b80426fef623e00fc
Bogdan <burcea.bogdan.madalin@gmail.com>
2018-11-17 23:14:04 +0200

    Fixed Environment to RandomEntry

--- src/main/java/ro/cburcea/hackathon/webapp/utils/RandomEntry.java
+++ src/main/java/ro/cburcea/hackathon/webapp/utils/RandomEntry.java
@@ -118,7 +118,7 @@ public class RandomEntry {
         categories.add("Salaries");
         categories.add("Interviews");
         categories.add("Trainings");
-        categories.add("Salaries");
+        categories.add("Environment");
 
         ArrayList<Company> companies = new ArrayList<>();
         companies.add( new Company("Endava", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", 30, "Bucharest", tags.get(0)) );
